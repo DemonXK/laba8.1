f9ce6e0d96e0f94bdb972cae015dcbd867c0f38e
Duke of Flies <demonxc050@gmail.com>
2025-05-26 01:46:10 +0300

    Удалена папка test

--- test/unit/conference_talk.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-struct ConferenceTalk {
-    std::string title;
-    int durationMinutes;  // продолжительность в минутах
-};
--- test/unit/processing.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vector>
-#include "conference_talk.h"  // структура с данными о докладах
-
-int findLongestTalkDuration(const std::vector<ConferenceTalk>& talks);
-
