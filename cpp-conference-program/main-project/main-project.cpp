585a4afdf6adae19ba9e7bf18f62d1b401341e7d
Duke of Flies <demonxc050@gmail.com>
2025-05-25 22:52:50 +0300

    Реализован вывод данных

---
+++ cpp-conference-program/main-project/main-project.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "file_reader.h"
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    auto entries = readDataFromFile("main-project/data.txt");
+    for (const auto& e : entries) {
+        std::cout << e.lastName << " " << e.firstName << " " << e.middleName
+            << ": " << e.topic << "\n";
+    }
+    return 0;
+}
